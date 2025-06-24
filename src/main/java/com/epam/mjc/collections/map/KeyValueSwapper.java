3d527e7ea74687f70d2d8089e2eda618bdae2e44
Mikita Buinavets <mikita.buinavets@arua.co>
2025-06-25 02:57:55 +0300

    Implement function value checking, key-value swapping, and word repetition mapping in respective classes

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,18 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> res = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            String value = entry.getValue();
+            Integer key = entry.getKey();
+            if (!res.containsKey(value) || res.get(value) > key) {
+                res.put(value, key);
+            }
+        }
+        return res;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,17 @@
 package com.epam.mjc.collections.map;
 
 import java.util.Map;
+import java.util.HashMap;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> res = new HashMap<>();
+        String[] words = sentence.toLowerCase().split("[^\\p{L}]+");
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                res.put(word, res.getOrDefault(word, 0) + 1);
+            }
+        }
+        return res;
     }
 }
